16c3b0b769831ddd9cd4b6d3227dc352cba27023
Alexia Desfontaines <alexia.desfontaines@ens.uvsq.fr>
2025-10-06 12:01:06 +0200

    Ajout du constructeur dans la classe AdresseIP

--- src/main/java/fr/uvsq/cprog/collex/AdresseIP.java
+++ src/main/java/fr/uvsq/cprog/collex/AdresseIP.java
@@ -1,5 +1,18 @@
 package fr.uvsq.cprog.collex;
 
+import java.util.Objects;
+
 public class AdresseIP {
-    
+    private  final String ip;
+
+    public AdresseIP(String ip) {
+        if (ip == null) {
+            throw new IllegalArgumentException("Adresse IP ne peut pas être nulle");
+        }
+        if (!ip.matches("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$")) {
+            throw new IllegalArgumentException("Adresse IP invalide : " + ip);
+        } 
+        this.ip = ip;
+    }
+
 }
