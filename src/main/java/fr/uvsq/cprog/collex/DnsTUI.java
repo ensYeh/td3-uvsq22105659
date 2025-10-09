6c39cd8c5d62cfd1ceac30430c9a00314aeefcc5
Alexia Desfontaines <alexia.desfontaines@ens.uvsq.fr>
2025-10-09 13:07:19 +0200

    Ajout de la méthode nextCommande() de la classe DnsTUI

--- src/main/java/fr/uvsq/cprog/collex/DnsTUI.java
+++ src/main/java/fr/uvsq/cprog/collex/DnsTUI.java
@@ -1,5 +1,15 @@
 package fr.uvsq.cprog.collex;
 
+import java.util.Scanner;
+import java.util.List;
+
 public class DnsTUI {
     
+    private final Scanner sc = new Scanner(System.in);
+
+    public Commande nextCommande() {
+        System.out.print("> ");
+        String ligne = sc.nextLine().trim();
+        return null;
+    }
 }
