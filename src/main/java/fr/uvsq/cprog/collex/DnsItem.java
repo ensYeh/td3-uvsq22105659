94bf6e8ffbc5aad64d847816685a062a7b51262d
Alexia Desfontaines <alexia.desfontaines@ens.uvsq.fr>
2025-10-09 10:48:34 +0200

    Ajout du constructeur de la classe DnsItem

--- src/main/java/fr/uvsq/cprog/collex/DnsItem.java
+++ src/main/java/fr/uvsq/cprog/collex/DnsItem.java
@@ -1,5 +1,17 @@
 package fr.uvsq.cprog.collex;
 
 public class DnsItem {
-    
+    private final AdresseIP  ip;
+    private final NomMachine nomMachine;
+
+    public DnsItem(AdresseIP ip, NomMachine nomMachine) {
+        if (ip == null) {
+            throw new NullPointerException("Adresse IP ne peut pas être nulle");
+        }
+        if (nomMachine == null) {
+            throw new NullPointerException("Nom de machine ne peut pas être nulle");
+        }
+        this.ip = ip;
+        this.nomMachine = nomMachine;
+    }
 }
