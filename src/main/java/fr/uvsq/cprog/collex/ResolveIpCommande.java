e498dab6fa5c0dadbdb0e60884b1d67677cb4074
Alexia Desfontaines <alexia.desfontaines@ens.uvsq.fr>
2025-10-10 20:18:00 +0200

    Ajout du constructeur de la classe ResolveIpCommande

--- src/main/java/fr/uvsq/cprog/collex/ResolveIpCommande.java
+++ src/main/java/fr/uvsq/cprog/collex/ResolveIpCommande.java
@@ -1,5 +1,10 @@
 package fr.uvsq.cprog.collex;
 
 public class ResolveIpCommande {
+    private final NomMachine nomMachine;
+
+    public ResolveIpCommande(NomMachine nomMachine) {
+        this.nomMachine = nomMachine;
+    }
     
 }
