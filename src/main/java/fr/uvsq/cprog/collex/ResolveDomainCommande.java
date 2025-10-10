05e059f4d27fa72d152835e68edcb845ce2e34ce
Alexia Desfontaines <alexia.desfontaines@ens.uvsq.fr>
2025-10-10 21:04:13 +0200

    Ajout du constructeur de la classe ResolveDomainCommande

--- src/main/java/fr/uvsq/cprog/collex/ResolveDomainCommande.java
+++ src/main/java/fr/uvsq/cprog/collex/ResolveDomainCommande.java
@@ -1,5 +1,9 @@
 package fr.uvsq.cprog.collex;
 
 public class ResolveDomainCommande implements Commande {
-    
+    private final String domaine;
+
+    public ResolveDomainCommande(String domaine) {
+        this.domaine = domaine;
+    }
 }
