d77d3520ce05a3e6e0ec6cd9ef5c8fd7c80d9548
Alexia Desfontaines <alexia.desfontaines@ens.uvsq.fr>
2025-10-10 21:47:38 +0200

    Modification de la méthode nextCommande() de la classe DnsTUI, afin que la bonne action soit exécutée en fonction de ce que demande l'utilisateur

--- src/main/java/fr/uvsq/cprog/collex/DnsTUI.java
+++ src/main/java/fr/uvsq/cprog/collex/DnsTUI.java
@@ -10,7 +10,41 @@ public class DnsTUI {
     public Commande nextCommande() {
         System.out.print("> ");
         String ligne = sc.nextLine().trim();
-        return null;
+        
+        if (ligne.isEmpty()) return null;
+
+        String[] parties = ligne.split("\\s+");
+
+        try {
+            if (parties[0].equalsIgnoreCase("add") && parties.length == 3) {
+                AdresseIP ip = new AdresseIP(parties[1]);
+                String[] nd = parties[2].split("\\.", 2);
+                NomMachine nom = new NomMachine(nd[0], nd[1]);
+                return new AddCommande(ip, nom);
+            }
+
+            if (parties[0].equalsIgnoreCase("ls")) {
+                boolean triParAdresse = (parties.length == 3 && parties[1].equals("-a"));
+                String domaine = triParAdresse ? parties[2] : parties[1];
+                return new ResolveDomainCommande(domaine);
+            }
+
+            if (parties[0].equalsIgnoreCase("quit")) {
+                return new QuitCommande();
+            }
+
+            if (parties[0].matches("((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$")) {
+                AdresseIP ip = new AdresseIP(parties[0]);
+                return new ResolveNameCommande(ip);
+            }
+
+            String[] nd = parties[0].split("\\.", 2);
+            NomMachine nm = new NomMachine(nd[0], nd[1]);
+            return new ResolveIpCommande(nm);
+        }  catch (Exception e) {
+            System.out.println("Commande invalide : " + e.getMessage());
+            return null;
+        }
     }
 
     public void affiche (Object resultat) {
