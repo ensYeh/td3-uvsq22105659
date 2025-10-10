4aa5b029d02dc9148aed0c38099312f204aae5e9
Alexia Desfontaines <alexia.desfontaines@ens.uvsq.fr>
2025-10-10 21:16:47 +0200

    Rédefinition de la méthode execute() dans la classe QuitCommande, de manière à ce que l'interface se ferme quand c'est demandé

--- src/main/java/fr/uvsq/cprog/collex/QuitCommande.java
+++ src/main/java/fr/uvsq/cprog/collex/QuitCommande.java
@@ -2,4 +2,10 @@ package fr.uvsq.cprog.collex;
 
 public class QuitCommande implements Commande {
     
+    @Override
+    public Object execute(Dns dns) {
+        System.out.println("Fermeture du programme...");
+        System.exit(0);
+        return null;
+    }
 }
