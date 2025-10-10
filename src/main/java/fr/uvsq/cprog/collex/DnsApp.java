34caa99a9abf1463619875231115bfafee71d7d4
Alexia Desfontaines <alexia.desfontaines@ens.uvsq.fr>
2025-10-10 21:59:15 +0200

    Ajout du constructeur dans la classe DnsApp

--- src/main/java/fr/uvsq/cprog/collex/DnsApp.java
+++ src/main/java/fr/uvsq/cprog/collex/DnsApp.java
@@ -1,5 +1,16 @@
 package fr.uvsq.cprog.collex;
 
+import java.io.IOException;
+import java.nio.file.Path;
+
 public class DnsApp {
+
+    private final Dns dns;
+    private final DnsTUI tui;
+
+    public DnsApp(Path fichier) throws IOException {
+        this.dns = new Dns(fichier);
+        this.tui = new DnsTUI();
+    }
     
 }
