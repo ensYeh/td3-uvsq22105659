b67aa8bdd502d62d076628464fe2b9c62f36f7be
Alexia Desfontaines <alexia.desfontaines@ens.uvsq.fr>
2025-10-10 20:22:18 +0200

    redéfinition de la méthode execute dans la classe ResolveIpCommande, afin d'obtenir l'adresse ip quand on fournit un nom de machine

--- src/main/java/fr/uvsq/cprog/collex/ResolveIpCommande.java
+++ src/main/java/fr/uvsq/cprog/collex/ResolveIpCommande.java
@@ -1,10 +1,20 @@
 package fr.uvsq.cprog.collex;
 
-public class ResolveIpCommande {
+public class ResolveIpCommande implements Commande{
     private final NomMachine nomMachine;
 
     public ResolveIpCommande(NomMachine nomMachine) {
         this.nomMachine = nomMachine;
+    }
+
+    @Override
+    public Object execute(Dns dns) {
+        DnsItem item = dns.getItem(nomMachine);
+        if (item != null) {
+            return item.getIp()
+        } else {
+            return "Aucun résultat pour cette machine";
+        }
     }
     
 }
