a034f422985631176dba1e8d274c481999dad6b1
Alexia Desfontaines <alexia.desfontaines@ens.uvsq.fr>
2025-10-10 20:30:49 +0200

    Ajout du contructeur de la classe ResolveNameCommande

--- src/main/java/fr/uvsq/cprog/collex/ResolveNameCommande.java
+++ src/main/java/fr/uvsq/cprog/collex/ResolveNameCommande.java
@@ -1,5 +1,10 @@
 package fr.uvsq.cprog.collex;
 
-public class ResolveNameCommande {
+public class ResolveNameCommande implements Commande{
+    private final AdresseIP ip;
+
+    public ResolveNameCommande(AdresseIP ip) {
+        this.ip = ip;
+    }
     
 }
