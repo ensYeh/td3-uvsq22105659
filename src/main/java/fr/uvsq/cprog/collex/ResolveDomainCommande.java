1db45b9aa17a16e3f7710b4416fff372f3ce4616
Alexia Desfontaines <alexia.desfontaines@ens.uvsq.fr>
2025-10-10 21:10:08 +0200

    Rédefinition de la méthode execute() dans la classe ResolveDomainCommande, afin de renvoyer toutes les machines appartenant au domaine indiqué

--- src/main/java/fr/uvsq/cprog/collex/ResolveDomainCommande.java
+++ src/main/java/fr/uvsq/cprog/collex/ResolveDomainCommande.java
@@ -1,9 +1,21 @@
 package fr.uvsq.cprog.collex;
 
+import java.util.List;
+
 public class ResolveDomainCommande implements Commande {
     private final String domaine;
 
     public ResolveDomainCommande(String domaine) {
         this.domaine = domaine;
+    }
+
+    @Override
+    public Object execute(Dns dns) {
+        List<DnsItem> items = dns.getItems(domaine);
+        if (items == null || items.isEmpty()) {
+            return "Aucun résultat pour ce domaine";
+        } else {
+            return items;
+        }
     }
 }
