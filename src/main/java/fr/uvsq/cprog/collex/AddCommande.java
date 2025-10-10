d6bdfa84c5e03a2e2c2698b76ebe849f7e00f0ea
Alexia Desfontaines <alexia.desfontaines@ens.uvsq.fr>
2025-10-10 20:09:49 +0200

    Ajout de la méthode qui permet de rajouter une adresse ip et un nom de domaine dans un fichier

--- src/main/java/fr/uvsq/cprog/collex/AddCommande.java
+++ src/main/java/fr/uvsq/cprog/collex/AddCommande.java
@@ -1,5 +1,7 @@
 package fr.uvsq.cprog.collex;
 
+import java.io.IOException;
+
 public class AddCommande implements Commande{
     private final AdresseIP ip;
     private final NomMachine  nomMachine;
@@ -10,5 +12,14 @@ public class AddCommande implements Commande{
     }
     
 
-    
+    @Override
+    public Object execute(Dns dns) {
+        try {
+            dns.addItem(ip, nomMachine);
+            return "Ajout effectué";
+        } catch (IOException | IllegalArgumentException e) {
+            return "Erreur lors de l'ajout : " + e.getMessage();
+        }
+    }
+
 }
